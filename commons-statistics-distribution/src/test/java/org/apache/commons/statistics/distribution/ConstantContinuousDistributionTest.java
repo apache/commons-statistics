9c794a15f75aafbe9d2ab4b62b7e43e1c32e7501
Gilles Sadowski <gilles@harfang.homelinux.org>
2018-01-21 14:41:03 +0100

    STATISTICS-2: Migrate "o.a.c.math4.distribution" from Commons Math.
    "IntegerDistribution" was renamed "DiscreteDistribution".
    "RealDistribution" was renamed "ContinuousDistribution".
    
    All exceptions are instances of "DistributionException" (package-private).
    
    Solver code (used by method "inverseCumulativeProbability") is a private
    static inner class in "AbstractContinuousDistribution".
    Tolerances are hard-coded. [Constructors that specified a tolerance were
    removed.]
    
    Calls to "FastMath" were replaced by calls to JDK "Math".  This has led to
    two unit tests failing in "GammaDistributionTest" for which the tolerance
    had to be slightly increased. [The main source indicates which calls to
    "Math" methods are responsible for the failures at the original tolerance.]

---
+++ commons-statistics-distribution/src/test/java/org/apache/commons/statistics/distribution/ConstantContinuousDistributionTest.java
@@ -0,0 +1,92 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.commons.statistics.distribution;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Test cases for ConstantContinuousDistribution.
+ */
+public class ConstantContinuousDistributionTest extends ContinuousDistributionAbstractTest {
+
+    // --- Override tolerance -------------------------------------------------
+
+    @Override
+    public void setUp() {
+        super.setUp();
+        setTolerance(0);
+    }
+
+    //--- Implementations for abstract methods --------------------------------
+
+    /** Creates the default uniform real distribution instance to use in tests. */
+    @Override
+    public ConstantContinuousDistribution makeDistribution() {
+        return new ConstantContinuousDistribution(1);
+    }
+
+    /** Creates the default cumulative probability distribution test input values */
+    @Override
+    public double[] makeCumulativeTestPoints() {
+        return new double[] {0, 0.5, 1};
+    }
+
+    /** Creates the default cumulative probability distribution test expected values */
+    @Override
+    public double[] makeCumulativeTestValues() {
+        return new double[] {0, 0, 1};
+    }
+
+    /** Creates the default probability density test expected values */
+    @Override
+    public double[] makeDensityTestValues() {
+        return new double[] {0, 0, 1};
+    }
+
+    /** Override default test, verifying that inverse cum is constant */
+    @Override
+    @Test
+    public void testInverseCumulativeProbabilities() {
+        ContinuousDistribution dist = getDistribution();
+        for (double x : getCumulativeTestValues()) {
+            Assert.assertEquals(1,dist.inverseCumulativeProbability(x), 0);
+        }
+    }
+
+    //--- Additional test cases -----------------------------------------------
+
+    @Test
+    public void testMeanVariance() {
+        ConstantContinuousDistribution dist;
+
+        dist = new ConstantContinuousDistribution(-1);
+        Assert.assertEquals(dist.getNumericalMean(), -1, 0d);
+        Assert.assertEquals(dist.getNumericalVariance(), 0, 0d);
+    }
+
+    @Test
+    @Override
+    public void testSampler() {
+        final double value = 12.345;
+        final ContinuousDistribution.Sampler sampler = new ConstantContinuousDistribution(value).createSampler(null);
+        for (int i = 0; i < 10; i++) {
+            Assert.assertEquals(value, sampler.sample(), 0);
+        }
+    }
+}
