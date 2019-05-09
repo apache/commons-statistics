9f16069c2a8c68bd612e055cab0218fcba44e47b
BBenNguyenn <BenN.Business@Gmail.com>
2019-05-09 15:55:31 -0500

    Setup JUnit and EJML dependencies and start SetupPractice file

---
+++ commons-statistics-regression/src/main/java/org/apache/commons/statistics/regression/SetupPractice.java
@@ -0,0 +1,11 @@
+package org.apache.commons.statistics.regression;
+
+public class SetupPractice {
+
+    public static void main(String[] args) {
+        System.out.println("Magic");
+    }
+
+
+
+}
