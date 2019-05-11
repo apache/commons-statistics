ebdce800ff7d5c6e140a7a7b471cd507ef6fcb61
BBenNguyenn <BenN.Business@Gmail.com>
2019-05-11 12:01:14 -0500

    EJML Learning and experimenting

--- commons-statistics-regression/src/main/java/org/apache/commons/statistics/regression/SetupPractice.java
+++ commons-statistics-regression/src/main/java/org/apache/commons/statistics/regression/SetupPractice.java
@@ -1,11 +1,47 @@
 package org.apache.commons.statistics.regression;
 
+import java.io.IOException;
+import java.util.Random;
+
+import org.ejml.data.DMatrix3;
+import org.ejml.data.DMatrix3x3;
+import org.ejml.data.DMatrixRMaj;
+import org.ejml.data.Matrix;
+import org.ejml.data.MatrixType;
+import org.ejml.dense.fixed.CommonOps_DDF3;
+import org.ejml.dense.row.RandomMatrices_DDRM;
+import org.ejml.ops.ConvertDMatrixStruct;
+import org.ejml.ops.MatrixIO;
+import org.ejml.simple.SimpleBase;
+import org.ejml.simple.SimpleMatrix;
+
 public class SetupPractice {
 
-    public static void main(String[] args) {
-        System.out.println("Magic");
-    }
+	public static void main(String[] args) {
+		System.out.println("Magic");
 
+		DMatrixRMaj A = new DMatrixRMaj(2, 3, true, new double[] { 1, 2, 3, 4, 5, 6 });
 
+		try {
+			MatrixIO.saveDenseCSV(A, "matrix_file.csv");
+			DMatrixRMaj B = MatrixIO.loadCSV("matrix_file.csv", false);
+			B.print();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+		
+		SimpleMatrix As = new SimpleMatrix(2, 3, true, new double[] { 1, 2, 3, 4, 5, 6 });
+
+		try {
+			As.saveToFileCSV("matrix_file.csv");
+			SimpleMatrix B = As.loadCSV("matrix_file.csv");
+			B.print();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+	
 
 }
---
+++ commons-statistics-regression/src/main/java/org/apache/commons/statistics/regression/StatisticsMatrix.java
@@ -0,0 +1,143 @@
+package org.apache.commons.statistics.regression;
+
+/*
+ * Copyright (c) 2009-2017, Peter Abeles. All Rights Reserved.
+ *
+ * This file is part of Efficient Java Matrix Library (EJML).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.ejml.data.DMatrixRMaj;
+import org.ejml.data.Matrix;
+import org.ejml.data.MatrixType;
+import org.ejml.dense.row.RandomMatrices_DDRM;
+import org.ejml.simple.SimpleBase;
+
+import java.util.Random;
+
+
+/**
+ * Example of how to extend "SimpleMatrix" and add your own functionality.  In this case
+ * two basic statistic operations are added.  Since SimpleBase is extended and StatisticsMatrix
+ * is specified as the generics type, all "SimpleMatrix" operations return a matrix of
+ * type StatisticsMatrix, ensuring strong typing.
+ *
+ * @author Peter Abeles
+ */
+public class StatisticsMatrix extends SimpleBase<StatisticsMatrix> {
+
+    public StatisticsMatrix( int numRows , int numCols ) {
+        super(numRows,numCols);
+    }
+
+    protected StatisticsMatrix(){}
+
+    /**
+     * Wraps a StatisticsMatrix around 'm'.  Does NOT create a copy of 'm' but saves a reference
+     * to it.
+     */
+    public static StatisticsMatrix wrap( DMatrixRMaj m ) {
+        StatisticsMatrix ret = new StatisticsMatrix();
+        ret.mat = m;
+
+        return ret;
+    }
+
+    /**
+     * Computes the mean or average of all the elements.
+     *
+     * @return mean
+     */
+    public double mean() {
+        double total = 0;
+
+        final int N = getNumElements();
+        for( int i = 0; i < N; i++ ) {
+            total += get(i);
+        }
+
+        return total/N;
+    }
+
+    /**
+     * Computes the unbiased standard deviation of all the elements.
+     *
+     * @return standard deviation
+     */
+    public double stdev() {
+        double m = mean();
+
+        double total = 0;
+
+        final int N = getNumElements();
+        if( N <= 1 )
+            throw new IllegalArgumentException("There must be more than one element to compute stdev");
+
+
+        for( int i = 0; i < N; i++ ) {
+            double x = get(i);
+
+            total += (x - m)*(x - m);
+        }
+
+        total /= (N-1);
+
+        return Math.sqrt(total);
+    }
+
+    /**
+     * Returns a matrix of StatisticsMatrix type so that SimpleMatrix functions create matrices
+     * of the correct type.
+     */
+    @Override
+    protected StatisticsMatrix createMatrix(int numRows, int numCols, MatrixType type) {
+        return new StatisticsMatrix(numRows,numCols);
+    }
+
+    public static void main( String args[] ) {
+        Random rand = new Random(24234);
+
+        int N = 500;
+
+        // create two vectors whose elements are drawn from uniform distributions
+        StatisticsMatrix A = StatisticsMatrix.wrap(RandomMatrices_DDRM.rectangle(N,1,0,1,rand));
+        StatisticsMatrix B = StatisticsMatrix.wrap(RandomMatrices_DDRM.rectangle(N,1,1,2,rand));
+
+        // the mean should be about 0.5
+        System.out.println("Mean of A is               "+A.mean());
+        // the mean should be about 1.5
+        System.out.println("Mean of B is               "+B.mean());
+
+        StatisticsMatrix C = A.plus(B);
+
+        // the mean should be about 2.0
+        System.out.println("Mean of C = A + B is       "+C.mean());
+
+        System.out.println("Standard deviation of A is "+A.stdev());
+        System.out.println("Standard deviation of B is "+B.stdev());
+        System.out.println("Standard deviation of C is "+C.stdev());
+    }
+
+//	@Override
+//	protected StatisticsMatrix createMatrix(int numRows, int numCols, MatrixType type) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+
+	@Override
+	protected StatisticsMatrix wrapMatrix(Matrix m) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
