7c254dfe6c9c790ec69ef72a064687648d2d2a99
BBenNguyenn <BenN.Business@Gmail.com>
2019-05-15 19:25:51 -0500

    Javadoc Test

--- commons-statistics-regression/src/main/java/org/apache/commons/statistics/regression/SetupPractice.java
+++ commons-statistics-regression/src/main/java/org/apache/commons/statistics/regression/SetupPractice.java
@@ -1,6 +1,10 @@
 package org.apache.commons.statistics.regression;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Random;
 
 import org.ejml.data.DMatrix3;
@@ -20,6 +24,33 @@ public class SetupPractice {
     public static void main(String[] args) {
         System.out.println("-- SETUP PRACTICE --");
 
+        
+        
+        
+        
+        List<String> list = new ArrayList<>();
+        list.add("One");
+        list.add("Abc");
+        list.add("BCD");
+
+        Collections.sort(list, (String a, String b) -> {
+            return a.compareTo(b);
+        });
+
+        System.out.println(list);
+        
+        
+        Comparator<String> comparator = (String a, String b) -> {
+            return a.compareTo(b);
+        };
+
+        Comparator<String> comparatorReversed = comparator.reversed();
+
+        Collections.sort(list, comparatorReversed);
+
+        System.out.println(list);
+        
+        
 //		DMatrixRMaj A = new DMatrixRMaj(2, 3, true, new double[] { 1, 2, 3, 4, 5, 6 });
 //
 //		try {
@@ -67,17 +98,25 @@ public class SetupPractice {
 //				
 //		}
 
-        double mat[][] = { { 1, 2, 3 }, { 7, 8, 9 } };
+        
+        
+        
+//        double mat[][] = { { 1, 2, 3 }, { 7, 8, 9 } };
+//
+//        for (int i = 0; i < mat.length; i++) {
+//            for (int j = 0; j < mat[i].length; j++) {
+//                System.out.println("Values at arr[" + i + "][" + j + "] is " + mat[i][j]);
+//            }
+//        }
+//
+//        SimpleMatrix A = new SimpleMatrix(mat);
+//        SimpleMatrix B = A.transpose();
 
-        for (int i = 0; i < mat.length; i++) {
-            for (int j = 0; j < mat[i].length; j++) {
-                System.out.println("Values at arr[" + i + "][" + j + "] is " + mat[i][j]);
-            }
-        }
-
-        SimpleMatrix A = new SimpleMatrix(mat);
-        SimpleMatrix B = A.transpose();
-
+        
+        
+        
+        
+        
         // create two vectors whose elements are drawn from uniform distributions
 //        SimpleMatrix A = SimpleMatrix.wrap(RandomMatrices_DDRM.rectangle(N,1,0,1,rand));
 //        SimpleMatrix B = SimpleMatrix.wrap(RandomMatrices_DDRM.rectangle(N,1,1,2,rand));
@@ -87,16 +126,24 @@ public class SetupPractice {
         // the mean should be about 1.5
 //        System.out.println("Mean of B is               "+B.mean());
 
-        SimpleMatrix C = A.mult(B);
+        
+        
+        
+        
+        
+//        SimpleMatrix C = A.mult(B);
+//
+//        // the mean should be about 2.0
+//        System.out.println();
+//        C.print();
+//        System.out.println();
+//        A.print();
+//        System.out.println();
+//        B.print();
 
-        // the mean should be about 2.0
-        System.out.println();
-        C.print();
-        System.out.println();
-        A.print();
-        System.out.println();
-        B.print();
-
+        
+        
+        
 //        System.out.println("Standard deviation of A is "+A.stdev());
 //        System.out.println("Standard deviation of B is "+B.stdev());
 //        System.out.println("Standard deviation of C is "+C.stdev());
