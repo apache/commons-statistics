d2150f80a4db9d9e9728c9d65e9e6d424effe70c
virendrasinghrp <virendrasinghrp@gmail.com>
2019-05-22 04:07:10 +0530

    STATISTICS-7- adding a new 'commons-statistics-descriptive' module/maven project for Descriptive Statistics

---
+++ commons-statistics-descriptive/src/main/java/org/apache/commons/statistics/descriptive/App.java
@@ -0,0 +1,13 @@
+package org.apache.commons.statistics.descriptive;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
