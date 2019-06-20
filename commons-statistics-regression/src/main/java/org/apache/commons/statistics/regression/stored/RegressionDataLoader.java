45bb1c22d7f6218e8271e4535d783c9bc60fbf6b
BBenNguyenn <BenN.Business@Gmail.com>
2019-06-20 12:05:31 -0500

    Updated newXData and newYData and started creating corresponding unit test

--- commons-statistics-regression/src/main/java/org/apache/commons/statistics/regression/stored/RegressionDataLoader.java
+++ commons-statistics-regression/src/main/java/org/apache/commons/statistics/regression/stored/RegressionDataLoader.java
@@ -1,47 +1,82 @@
 package org.apache.commons.statistics.regression.stored;
 
+import org.apache.commons.statistics.regression.util.array.ArrayUtils;
 import org.apache.commons.statistics.regression.util.matrix.StatisticsMatrix;
 import org.ejml.data.DMatrixRMaj;
 
 public class RegressionDataLoader {
-    
+
     private RegressionRawData inputData;
-    
-    public RegressionDataLoader(double [] y, double [][] x) {
+
+    public RegressionDataLoader(double[] y, double[][] x) {
+        validateSampleData(y, x);
         inputData = new RegressionRawData(newY(y), newX(x), false);
     }
-    
+
     public RegressionDataLoader() {
         inputData = new RegressionRawData();
     }
-    
-    public void newSampleData(double [] y, double [][] x) {
-        if(inputData == null) 
+
+    public void newSampleData(double[] y, double[][] x) {
+        validateSampleData(y, x);
+        if (inputData == null)
             inputData = new RegressionRawData(newY(y), newX(x), false);
-        
+
         else {
             inputData.setYData(newY(y));
             inputData.setXData(newX(x));
         }
     }
-    
-    public void newYData(double [] y) {
-        inputData.setYData(newY(y));
+
+    public void newYData(double[] y) {
+        if (inputData != null) {
+            if (inputData.getXData() != null) {
+
+                validateSampleData(y, ArrayUtils.matrixToArray2D(inputData.getXData()));
+                inputData.setYData(newY(y));
+            } else
+                throw new NullPointerException(
+                        "X matrix is null, please use newSampleData(y, x) to input initial data.");
+        } else
+            throw new NullPointerException(); // not reachable?
     }
-    
-    public void newXData(double [][] x) {
-        inputData.setXData(newX(x));
+
+    public void newXData(double[][] x) {
+        if (inputData != null) {
+            if (inputData.getYData() != null) {
+
+                validateSampleData(ArrayUtils.matrixToArray1D(inputData.getYData()), x);
+                inputData.setXData(newX(x));
+            } else
+                throw new NullPointerException(
+                        "Y matrix is null, please use newSampleData(y, x) to input initial data.");
+        } else
+            throw new NullPointerException(); // not reachable?
     }
-    
-    private static StatisticsMatrix newY(double [] y) {
+
+    protected void validateSampleData(double[] y, double[][] x) throws IllegalArgumentException {
+        if ((x == null) || (y == null)) {
+            throw new IllegalArgumentException("Null argument(s).");
+        }
+        if (x.length != y.length) {
+            throw new IllegalArgumentException("Dimension mismatch: " + y.length + " != " + x.length);
+        }
+        if (x.length == 0) { // Must be no y data either
+            throw new IllegalArgumentException("Arrays have length 0.");
+        }
+        if (x[0].length + 1 > x.length) {
+            throw new IllegalArgumentException("Not enough data for number of predictors.");
+        }
+    }
+
+    private static StatisticsMatrix newY(double[] y) {
         return (new StatisticsMatrix(new DMatrixRMaj(y)));
     }
-    
 
-    private static StatisticsMatrix newX(double [][] x) {
+    private static StatisticsMatrix newX(double[][] x) {
         return (new StatisticsMatrix(new DMatrixRMaj(x)));
     }
-    
+
     public RegressionData getInputData() {
         return inputData;
     }
--- commons-statistics-regression/src/test/java/org/apache/commons/statistics/regression/stored/RegressionDataLoaderTest.java
+++ commons-statistics-regression/src/test/java/org/apache/commons/statistics/regression/stored/RegressionDataLoaderTest.java
@@ -10,27 +10,40 @@ import org.junit.Test;
 
 public class RegressionDataLoaderTest {
 
+    private double[] yData = new double[] { 10, 11, 12, 13 };
+    private double[][] xData = new double[][] { { -2, -1, 0 }, { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+    private double[] yData2 = new double[] { 10.2, 11.2, 12.2 };
+    private double[][] xData2 = new double[][] { { 1.2, 2.2, 3.2 }, { 4.2, 5.2, 6.2 }, { 7.2, 8.2, 9.2 } };
+
     @Test
-    public void loadingDataTest() {
-
-        double[] yData = new double[] { 10, 11, 12 };
-        double[][] xData = new double[][] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
-
+    public void newSampleDataTest() {
         RegressionDataLoader data = new RegressionDataLoader();
         data.newSampleData(yData, xData);
 
-        // Printing the testing arrays, before and after wrapped inside a StatisticsMatrix object
+        // Printing the testing arrays, before and after wrapped inside a
+        // StatisticsMatrix object
 //        ArrayUtils.printArrayWithStreams(yData);
 //        ArrayUtils.printArrayWithStreams(xData);
 //        ArrayUtils.printArrayWithStreams(ArrayUtils.matrixToArray1D(data.getInputData().getYData()));
 //        ArrayUtils.printArrayWithStreams(ArrayUtils.matrixToArray2D(data.getInputData().getXData()));
 
         assertEquals(data.getInputData().getYData().get(1), 11, 0);
-        assertEquals(data.getInputData().getXData().get(0, 0), 1, 0);
-        assertEquals(data.getInputData().getXData().get(1, 1), 5, 0);
+        assertEquals(data.getInputData().getXData().get(0, 0), -2, 0);
+        assertEquals(data.getInputData().getXData().get(1, 1), 2, 0);
 
         assertTrue(Arrays.equals(ArrayUtils.matrixToArray1D(data.getInputData().getYData()), yData));
         assertArrayEquals(ArrayUtils.matrixToArray2D(data.getInputData().getXData()), xData);
+
+    }
+
+    @Test
+    public void newYDataTest() {
+
+    }
+
+    @Test
+    public void newXDataTest() {
 
     }
 
