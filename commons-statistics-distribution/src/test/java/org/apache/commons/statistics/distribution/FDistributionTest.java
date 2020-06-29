fb40553df59940377a2193e20b7a4cebbfb27e83
aherbert <aherbert@apache.org>
2020-06-29 14:26:36 +0100

    Fix SonarCloud: Assertion should contain the error message.

--- commons-statistics-distribution/src/test/java/org/apache/commons/statistics/distribution/FDistributionTest.java
+++ commons-statistics-distribution/src/test/java/org/apache/commons/statistics/distribution/FDistributionTest.java
@@ -137,14 +137,9 @@ public class FDistributionTest extends ContinuousDistributionAbstractTest {
     @Test
     public void testMath785() {
         // this test was failing due to inaccurate results from ContinuedFraction.
-
-        try {
-            final double prob = 0.01;
-            final FDistribution f = new FDistribution(200000, 200000);
-            final double result = f.inverseCumulativeProbability(prob);
-            Assertions.assertTrue(result < 1.0);
-        } catch (final AssertionError ex) {
-            Assertions.fail("Failing to calculate inverse cumulative probability");
-        }
+        final double prob = 0.01;
+        final FDistribution f = new FDistribution(200000, 200000);
+        final double result = f.inverseCumulativeProbability(prob);
+        Assertions.assertTrue(result < 1.0, "Failing to calculate inverse cumulative probability");
     }
 }
