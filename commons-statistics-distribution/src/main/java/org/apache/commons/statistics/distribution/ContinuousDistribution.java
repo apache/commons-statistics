314966fbabbd9e63098af9dea70bab777a83596f
aherbert <aherbert@apache.org>
2021-07-30 12:09:53 +0100

    Javadoc formatting

--- commons-statistics-distribution/src/main/java/org/apache/commons/statistics/distribution/ContinuousDistribution.java
+++ commons-statistics-distribution/src/main/java/org/apache/commons/statistics/distribution/ContinuousDistribution.java
@@ -62,7 +62,7 @@ public interface ContinuousDistribution {
      * In general, the PDF is the derivative of the {@link #cumulativeProbability(double) CDF}.
      * If the derivative does not exist at {@code x}, then an appropriate
      * replacement should be returned, e.g. {@code Double.POSITIVE_INFINITY},
-     * {@code Double.NaN}, or  the limit inferior or limit superior of the
+     * {@code Double.NaN}, or the limit inferior or limit superior of the
      * difference quotient.
      *
      * @param x Point at which the PDF is evaluated.
