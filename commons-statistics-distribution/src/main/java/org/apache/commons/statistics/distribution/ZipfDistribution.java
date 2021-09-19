40d46f120ec8c7111e22f78c6cd710ab3b0dcd6f
Alex Herbert <aherbert@apache.org>
2021-09-19 09:26:36 +0100

    Avoid divide by using negation of exponent

--- commons-statistics-distribution/src/main/java/org/apache/commons/statistics/distribution/ZipfDistribution.java
+++ commons-statistics-distribution/src/main/java/org/apache/commons/statistics/distribution/ZipfDistribution.java
@@ -93,7 +93,7 @@ public class ZipfDistribution extends AbstractDiscreteDistribution {
             return 0;
         }
 
-        return (1 / Math.pow(x, exponent)) / nthHarmonic;
+        return Math.pow(x, -exponent) / nthHarmonic;
     }
 
     /** {@inheritDoc} */
