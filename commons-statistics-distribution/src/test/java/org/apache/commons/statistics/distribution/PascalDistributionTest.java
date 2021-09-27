56bbf6416683e2a6acc5931af94244639764440c
aherbert <aherbert@apache.org>
2021-09-27 12:55:36 +0100

    Update base test class for Pascal distribution

--- commons-statistics-distribution/src/test/java/org/apache/commons/statistics/distribution/PascalDistributionTest.java
+++ commons-statistics-distribution/src/test/java/org/apache/commons/statistics/distribution/PascalDistributionTest.java
@@ -16,149 +16,54 @@
  */
 package org.apache.commons.statistics.distribution;
 
-import org.apache.commons.rng.simple.RandomSource;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 /**
- * Test cases for PascalDistribution.
- * Extends DiscreteDistributionAbstractTest.  See class javadoc for
- * DiscreteDistributionAbstractTest for details.
+ * Test cases for {@link PascalDistribution}.
+ * Extends {@link BaseDiscreteDistributionTest}. See javadoc of that class for details.
  */
-class PascalDistributionTest extends DiscreteDistributionAbstractTest {
-
-    private static final double DEFAULT_TOLERANCE = 1e-7;
-
-    //---------------------- Override tolerance --------------------------------
-
-    @BeforeEach
-    void customSetUp() {
-        setTolerance(DEFAULT_TOLERANCE);
-    }
-
-    //-------------- Implementations for abstract methods ----------------------
-
+class PascalDistributionTest extends BaseDiscreteDistributionTest {
     @Override
-    public PascalDistribution makeDistribution() {
-        return new PascalDistribution(10, 0.70);
+    DiscreteDistribution makeDistribution(Object... parameters) {
+        final int r = (Integer) parameters[0];
+        final double p = (Double) parameters[1];
+        return new PascalDistribution(r, p);
     }
 
     @Override
-    public int[] makeProbabilityTestPoints() {
-        return new int[] {-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+    protected double getTolerance() {
+        return 1e-12;
     }
 
     @Override
-    public double[] makeProbabilityTestValues() {
-        return new double[] {0, 0.0282475249, 0.0847425747, 0.139825248255, 0.167790297906, 0.163595540458,
-                             0.137420253985, 0.103065190489, 0.070673273478, 0.0450542118422, 0.0270325271053,
-                             0.0154085404500, 0.0084046584273};
+    Object[][] makeInvalidParameters() {
+        return new Object[][] {
+            {0, 0.5},
+            {-1, 0.5},
+            {3, -0.1},
+            {3, 0.0},
+            {3, 1.1},
+        };
     }
 
     @Override
-    public int[] makeCumulativeTestPoints() {
-        return makeProbabilityTestPoints();
-    }
-
-    @Override
-    public double[] makeCumulativeTestValues() {
-        return new double[] {0, 0.0282475249, 0.1129900996, 0.252815347855, 0.420605645761, 0.584201186219,
-                             0.721621440204, 0.824686630693, 0.895359904171, 0.940414116013, 0.967446643119,
-                             0.982855183569, 0.991259841996};
-    }
-
-    @Override
-    public double[] makeInverseCumulativeTestPoints() {
-        return new double[] {0.0, 0.001, 0.010, 0.025, 0.050, 0.100, 0.999,
-                             0.990, 0.975, 0.950, 0.900, 1.0};
-    }
-
-    @Override
-    public int[] makeInverseCumulativeTestValues() {
-        return new int[] {0, 0, 0, 0, 1, 1, 14, 11, 10, 9, 8, Integer.MAX_VALUE};
-    }
-
-    @Override
-    public int[] makeSurvivalPrecisionTestPoints() {
-        return new int[] {47, 52};
-    }
-
-    @Override
-    public double[] makeSurvivalPrecisionTestValues() {
-        // computed using R version 3.4.4
-        return new double[] {3.1403888119656772712e-17, 1.7075879020163069251e-19};
+    String[] getParameterNames() {
+        return new String[] {"NumberOfSuccesses", "ProbabilityOfSuccess"};
     }
 
     //-------------------- Additional test cases -------------------------------
 
-    /** Test degenerate case p = 1. */
     @Test
-    void testDegenerate1() {
-        final PascalDistribution dist = new PascalDistribution(5, 1.0d);
-        Assertions.assertEquals(1.0, dist.getProbabilityOfSuccess());
-        Assertions.assertEquals(0.0, dist.getMean());
-        Assertions.assertEquals(0.0, dist.getVariance());
-
-        setDistribution(dist);
-        setProbabilityTestPoints(new int[] {-1, 0, 1, 2, 5, 10});
-        setProbabilityTestValues(new double[] {0.0, 1.0, 0.0, 0.0, 0.0, 0.0});
-        setCumulativeTestPoints(new int[] {-1, 0, 1, 2, 5, 10});
-        setCumulativeTestValues(new double[] {0.0, 1.0, 1.0, 1.0, 1.0, 1.0});
-        setInverseCumulativeTestPoints(new double[] {0.1, 0.5, 1.0});
-        setInverseCumulativeTestValues(new int[] {0, 0, 0});
-        verifyProbabilities();
-        verifyLogProbabilities();
-        verifyCumulativeProbabilities();
-        verifySurvivalProbability();
-        verifySurvivalAndCumulativeProbabilityComplement();
-        verifyInverseCumulativeProbabilities();
-
-        Assertions.assertEquals(0, dist.getSupportLowerBound());
-        Assertions.assertEquals(0, dist.getSupportUpperBound());
-
-        final DiscreteDistribution.Sampler s = dist.createSampler(RandomSource.SPLIT_MIX_64.create());
-        for (int i = 0; i < 5; i++) {
-            Assertions.assertEquals(0, s.sample());
-        }
-    }
-
-    @ParameterizedTest
-    @CsvSource({
-        "10, 0.7",
-        "12, 0.5",
-    })
-    void testParameterAccessors(int r, double p) {
-        final PascalDistribution dist = new PascalDistribution(r, p);
-        Assertions.assertEquals(r, dist.getNumberOfSuccesses());
-        Assertions.assertEquals(p, dist.getProbabilityOfSuccess());
-    }
-
-    @ParameterizedTest
-    @CsvSource({
-        "0, 0.5",
-        "-1, 0.5",
-        "3, -0.1",
-        "3, 0.0",
-        "3, 1.1",
-    })
-    void testConstructorPreconditions(int r, double p) {
-        Assertions.assertThrows(DistributionException.class, () -> new PascalDistribution(r, p));
-    }
-
-    @Test
-    void testMoments() {
-        final double tol = 1e-9;
+    void testAdditionalMoments() {
         PascalDistribution dist;
 
         dist = new PascalDistribution(10, 0.5);
-        Assertions.assertEquals((10d * 0.5d) / 0.5, dist.getMean(), tol);
-        Assertions.assertEquals((10d * 0.5d) / (0.5d * 0.5d), dist.getVariance(), tol);
+        Assertions.assertEquals((10d * 0.5d) / 0.5, dist.getMean(), getTolerance());
+        Assertions.assertEquals((10d * 0.5d) / (0.5d * 0.5d), dist.getVariance(), getTolerance());
 
         dist = new PascalDistribution(25, 0.7);
-        Assertions.assertEquals((25d * 0.3d) / 0.7, dist.getMean(), tol);
-        Assertions.assertEquals((25d * 0.3d) / (0.7d * 0.7d), dist.getVariance(), tol);
+        Assertions.assertEquals((25d * 0.3d) / 0.7, dist.getMean(), getTolerance());
+        Assertions.assertEquals((25d * 0.3d) / (0.7d * 0.7d), dist.getVariance(), getTolerance());
     }
 }
