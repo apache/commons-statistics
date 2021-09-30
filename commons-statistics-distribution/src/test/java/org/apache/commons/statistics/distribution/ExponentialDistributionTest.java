31e2a86239e400d112e82edeb94b151a9406bf3b
Alex Herbert <aherbert@apache.org>
2021-09-30 16:32:53 +0100

    Update base test class for exponential distribution

--- commons-statistics-distribution/src/test/java/org/apache/commons/statistics/distribution/ExponentialDistributionTest.java
+++ commons-statistics-distribution/src/test/java/org/apache/commons/statistics/distribution/ExponentialDistributionTest.java
@@ -18,100 +18,47 @@ package org.apache.commons.statistics.distribution;
 
 import org.apache.commons.numbers.core.Precision;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 /**
- * Test cases for ExponentialDistribution.
- * Extends ContinuousDistributionAbstractTest.  See class javadoc for
- * ContinuousDistributionAbstractTest for details.
+ * Test cases for {@link ExponentialDistribution}.
+ * Extends {@link BaseContinuousDistributionTest}. See javadoc of that class for details.
  */
-class ExponentialDistributionTest extends ContinuousDistributionAbstractTest {
-
-    //---------------------- Override tolerance --------------------------------
-
-    @BeforeEach
-    void customSetUp() {
-        setTolerance(1e-9);
-    }
-
-    //-------------- Implementations for abstract methods ----------------------
-
+class ExponentialDistributionTest extends BaseContinuousDistributionTest {
     @Override
-    public ExponentialDistribution makeDistribution() {
-        return new ExponentialDistribution(5.0);
+    ContinuousDistribution makeDistribution(Object... parameters) {
+        final double mean = (Double) parameters[0];
+        return new ExponentialDistribution(mean);
     }
 
     @Override
-    public double[] makeCumulativeTestPoints() {
-        // quantiles computed using R version 2.9.2
-        return new double[] {0.00500250166792, 0.0502516792675, 0.126589039921, 0.256466471938,
-                             0.526802578289, 34.5387763949, 23.0258509299, 18.4443972706, 14.9786613678, 11.5129254650};
+    protected double getTolerance() {
+        return 1e-10;
     }
 
     @Override
-    public double[] makeCumulativeTestValues() {
-        return new double[] {0.001, 0.01, 0.025, 0.05, 0.1, 0.999,
-                             0.990, 0.975, 0.950, 0.900};
+    Object[][] makeInvalidParameters() {
+        return new Object[][] {
+            {0.0},
+            {-0.1}
+        };
     }
 
     @Override
-    public double[] makeDensityTestValues() {
-        return new double[] {0.1998, 0.198, 0.195, 0.19, 0.18, 0.000200000000000,
-                             0.00200000000002, 0.00499999999997, 0.00999999999994, 0.0199999999999};
-    }
-
-    @Override
-    public double[] makeCumulativePrecisionTestPoints() {
-        return new double[] {1e-15, 4e-16, 9e-16};
-    }
-
-    @Override
-    public double[] makeCumulativePrecisionTestValues() {
-        // calculated via scipy, specifically expon.cdf(x/5).
-        // WolframAlpha provided either too accurate or inaccurate values.
-        return new double[] {2.0000000000000002e-16, 7.999999999999999e-17, 1.8000000000000002e-16};
-    }
-
-    @Override
-    public double[] makeSurvivalPrecisionTestPoints() {
-        return new double[] {183, 197};
-    }
-
-    @Override
-    public double[] makeSurvivalPrecisionTestValues() {
-        // These were created using WolframAlpha. NOTE lambda parameter is 1/mean
-        return new double[] {1.2729811194234181e-16, 7.741006159285781e-18};
+    String[] getParameterNames() {
+        return new String[] {"Mean"};
     }
 
     //------------ Additional tests -------------------------------------------
 
     @Test
-    void testCumulativeProbabilityExtremes() {
-        setDistribution(makeDistribution());
-        setCumulativeTestPoints(new double[] {-2, 0});
-        setCumulativeTestValues(new double[] {0, 0});
-        verifyCumulativeProbabilities();
+    void testProbabilityRange() {
+        final double actual = new ExponentialDistribution(5).probability(0.25, 0.75);
+        Assertions.assertEquals(0.0905214480756562, actual, getTolerance());
     }
 
     @Test
-    void testInverseCumulativeProbabilityExtremes() {
-        setDistribution(makeDistribution());
-        setInverseCumulativeTestPoints(new double[] {0, 1});
-        setInverseCumulativeTestValues(new double[] {0, Double.POSITIVE_INFINITY});
-        verifyInverseCumulativeProbabilities();
-    }
-
-    @Test
-    void testCumulativeProbability2() {
-        final double actual = makeDistribution().probability(0.25, 0.75);
-        Assertions.assertEquals(0.0905214, actual, 10e-4);
-    }
-
-    @Test
-    void testDensity() {
+    void testAdditionalDensity() {
         final ExponentialDistribution d1 = new ExponentialDistribution(1);
         Assertions.assertTrue(Precision.equals(0.0, d1.density(-1e-9), 1));
         Assertions.assertTrue(Precision.equals(1.0, d1.density(0.0), 1));
@@ -126,19 +73,5 @@ class ExponentialDistributionTest extends ContinuousDistributionAbstractTest {
 
         // computed using  print(dexp(2, rate=1/3), digits=10) in R 2.5
         Assertions.assertEquals(0.1711390397, d2.density(2.0), 1e-8);
-    }
-
-    @ParameterizedTest
-    @ValueSource(doubles = {0, -0.1})
-    void testConstructorPrecondition(double mean) {
-        Assertions.assertThrows(DistributionException.class, () -> new ExponentialDistribution(mean));
-    }
-
-    @ParameterizedTest
-    @ValueSource(doubles = {11, 10.5})
-    void testMoments(double mean) {
-        final ExponentialDistribution dist = new ExponentialDistribution(mean);
-        Assertions.assertEquals(mean, dist.getMean());
-        Assertions.assertEquals(mean * mean, dist.getVariance());
     }
 }
