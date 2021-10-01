c89b17de341a63adbdf7d8177f0408e43699b4a7
Alex Herbert <aherbert@apache.org>
2021-10-02 00:02:35 +0100

    Update base test class for t distribution

--- commons-statistics-distribution/src/test/java/org/apache/commons/statistics/distribution/TDistributionTest.java
+++ commons-statistics-distribution/src/test/java/org/apache/commons/statistics/distribution/TDistributionTest.java
@@ -17,70 +17,35 @@
 package org.apache.commons.statistics.distribution;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+
 /**
- * Test cases for TDistribution.
- * Extends ContinuousDistributionAbstractTest.  See class javadoc for
- * ContinuousDistributionAbstractTest for details.
+ * Test cases for {@link TDistribution}.
+ * Extends {@link BaseContinuousDistributionTest}. See javadoc of that class for details.
  */
-class TDistributionTest extends ContinuousDistributionAbstractTest {
-
-    //---------------------- Override tolerance --------------------------------
-
-    @BeforeEach
-    void customSetUp() {
-        setTolerance(1e-9);
-    }
-
-    //-------------- Implementations for abstract methods ----------------------
-
+class TDistributionTest extends BaseContinuousDistributionTest {
     @Override
-    public TDistribution makeDistribution() {
-        return new TDistribution(5.0);
+    ContinuousDistribution makeDistribution(Object... parameters) {
+        final double df = (Double) parameters[0];
+        return new TDistribution(df);
     }
 
     @Override
-    public double[] makeCumulativeTestPoints() {
-        // quantiles computed using R version 2.9.2
-        return new double[] {-5.89342953136, -3.36492999891, -2.57058183564, -2.01504837333, -1.47588404882,
-                             5.89342953136, 3.36492999891, 2.57058183564, 2.01504837333, 1.47588404882};
+    protected double getTolerance() {
+        return 1e-11;
     }
 
     @Override
-    public double[] makeCumulativeTestValues() {
-        return new double[] {0.001, 0.01, 0.025, 0.05, 0.1, 0.999,
-                             0.990, 0.975, 0.950, 0.900};
+    Object[][] makeInvalidParameters() {
+        return new Object[][] {
+            {0.0},
+            {-0.1}
+        };
     }
 
     @Override
-    public double[] makeDensityTestValues() {
-        return new double[] {0.000756494565517, 0.0109109752919, 0.0303377878006, 0.0637967988952, 0.128289492005,
-                             0.000756494565517, 0.0109109752919, 0.0303377878006, 0.0637967988952, 0.128289492005};
-    }
-
-    @Override
-    public double[] makeCumulativePrecisionTestPoints() {
-        return new double[] {-4200, -6400};
-    }
-
-    @Override
-    public double[] makeCumulativePrecisionTestValues() {
-        // These were created using WolframAlpha
-        return new double[] {7.261513520181652e-18, 8.838404680725267e-19};
-    }
-
-    @Override
-    public double[] makeSurvivalPrecisionTestPoints() {
-        return new double[] {4200, 6400};
-    }
-
-    @Override
-    public double[] makeSurvivalPrecisionTestValues() {
-        // These were created using WolframAlpha
-        return new double[] {7.261513520181652e-18, 8.838404680725267e-19};
+    String[] getParameterNames() {
+        return new String[] {"DegreesOfFreedom"};
     }
 
     //-------------------- Additional test cases -------------------------------
@@ -91,82 +56,28 @@ class TDistributionTest extends ContinuousDistributionAbstractTest {
      */
     @Test
     void testCumulativeProbabilityAgainstStackOverflow() {
+        final TDistribution td = new TDistribution(5.);
         Assertions.assertDoesNotThrow(() -> {
-            final TDistribution td = new TDistribution(5.);
             td.cumulativeProbability(.1);
             td.cumulativeProbability(.01);
         });
     }
 
     @Test
-    void testSmallDf() {
-        setDistribution(new TDistribution(1d));
-        // quantiles computed using R version 2.9.2
-        setCumulativeTestPoints(new double[] {-318.308838986, -31.8205159538, -12.7062047362,
-                                              -6.31375151468, -3.07768353718, 318.308838986,
-                                              31.8205159538, 12.7062047362, 6.31375151468,
-                                              3.07768353718});
-        setCumulativeTestValues(makeCumulativeTestValues());
-        setDensityTestValues(new double[] {3.14158231817e-06, 0.000314055924703, 0.00195946145194,
-                                           0.00778959736375, 0.0303958893917, 3.14158231817e-06,
-                                           0.000314055924703, 0.00195946145194, 0.00778959736375,
-                                           0.0303958893917});
-        setInverseCumulativeTestPoints(getCumulativeTestValues());
-        setInverseCumulativeTestValues(getCumulativeTestPoints());
-        verifyCumulativeProbabilities();
-        verifyInverseCumulativeProbabilities();
-        verifyDensities();
-    }
-
-    @Test
-    void testInverseCumulativeProbabilityExtremes() {
-        setDistribution(makeDistribution());
-        setInverseCumulativeTestPoints(new double[] {0, 1});
-        setInverseCumulativeTestValues(new double[] {Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY});
-        verifyInverseCumulativeProbabilities();
-    }
-
-    @Test
-    void testCumulativeProbablilityExtremes() {
+    void testAdditionalMoments() {
         TDistribution dist;
-        for (int i = 1; i < 11; i++) {
-            dist = new TDistribution(i * 5);
-            Assertions.assertEquals(1,
-                                dist.cumulativeProbability(Double.POSITIVE_INFINITY), Double.MIN_VALUE);
-            Assertions.assertEquals(0,
-                                dist.cumulativeProbability(Double.NEGATIVE_INFINITY), Double.MIN_VALUE);
-        }
-    }
-
-    @ParameterizedTest
-    @ValueSource(doubles = {1, 12.3})
-    void testParameterAccessors(double df) {
-        final TDistribution dist = new TDistribution(df);
-        Assertions.assertEquals(df, dist.getDegreesOfFreedom());
-    }
-
-    @ParameterizedTest
-    @ValueSource(doubles = {0, -0.1})
-    void testConstructorPrecondition(double df) {
-        Assertions.assertThrows(DistributionException.class, () -> new TDistribution(df));
-    }
-
-    @Test
-    void testMoments() {
-        final double tol = 1e-9;
-        TDistribution dist;
-
-        dist = new TDistribution(1);
-        Assertions.assertTrue(Double.isNaN(dist.getMean()));
-        Assertions.assertTrue(Double.isNaN(dist.getVariance()));
 
         dist = new TDistribution(1.5);
-        Assertions.assertEquals(0, dist.getMean(), tol);
+        Assertions.assertEquals(0, dist.getMean());
         Assertions.assertEquals(Double.POSITIVE_INFINITY, dist.getVariance());
 
-        dist = new TDistribution(5);
-        Assertions.assertEquals(0, dist.getMean(), tol);
-        Assertions.assertEquals(5d / (5d - 2d), dist.getVariance(), tol);
+        dist = new TDistribution(2.1);
+        Assertions.assertEquals(0, dist.getMean());
+        Assertions.assertEquals(2.1 / (2.1 - 2.0), dist.getVariance());
+
+        dist = new TDistribution(12.1);
+        Assertions.assertEquals(0, dist.getMean());
+        Assertions.assertEquals(12.1 / (12.1 - 2.0), dist.getVariance());
     }
 
     /*
@@ -182,10 +93,10 @@ class TDistributionTest extends ContinuousDistributionAbstractTest {
         final double[] args10 = new double[]{1.372, 1.812, 2.228, 2.764, 3.169, 4.143};
         final double[] args30 = new double[]{1.310, 1.697, 2.042, 2.457, 2.750, 3.385};
         final double[] args100 = new double[]{1.290, 1.660, 1.984, 2.364, 2.626, 3.174};
-        TestUtils.assertEquals(prob, makeNistResults(args2, 2), 1.0e-4);
-        TestUtils.assertEquals(prob, makeNistResults(args10, 10), 1.0e-4);
-        TestUtils.assertEquals(prob, makeNistResults(args30, 30), 1.0e-4);
-        TestUtils.assertEquals(prob, makeNistResults(args100, 100), 1.0e-4);
+        testSurvivalProbability(new TDistribution(2), args2, prob, 1e-4);
+        testSurvivalProbability(new TDistribution(10), args10, prob, 1e-4);
+        testSurvivalProbability(new TDistribution(30), args30, prob, 1e-4);
+        testSurvivalProbability(new TDistribution(100), args100, prob, 1e-4);
     }
 
     // See https://issues.apache.org/jira/browse/STATISTICS-25
@@ -219,14 +130,5 @@ class TDistributionTest extends ContinuousDistributionAbstractTest {
             final double x = dist.cumulativeProbability(c);
             Assertions.assertEquals(expected[i], x, tol);
         }
-    }
-
-    private double[] makeNistResults(double[] args, int df) {
-        final TDistribution td =  new TDistribution(df);
-        final double[] res  = new double[args.length];
-        for (int i = 0; i < res.length; i++) {
-            res[i] = 1.0 - td.cumulativeProbability(args[i]);
-        }
-        return res;
     }
 }
