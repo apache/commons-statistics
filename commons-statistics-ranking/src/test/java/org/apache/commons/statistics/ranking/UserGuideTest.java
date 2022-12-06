b0785598914123f682280657aae5cc5b3b963594
aherbert <aherbert@apache.org>
2022-12-06 17:29:34 +0000

    Add ranking module to the user guide.

--- commons-statistics-ranking/src/test/java/org/apache/commons/statistics/ranking/UserGuideTest.java
+++ commons-statistics-ranking/src/test/java/org/apache/commons/statistics/ranking/UserGuideTest.java
@@ -17,6 +17,10 @@
 
 package org.apache.commons.statistics.ranking;
 
+import java.util.Arrays;
+import java.util.SplittableRandom;
+import org.apache.commons.rng.UniformRandomProvider;
+import org.apache.commons.rng.simple.RandomSource;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -25,10 +29,48 @@ import org.junit.jupiter.api.Test;
  */
 class UserGuideTest {
     @Test
-    void testRanking() {
-        NaturalRanking ranking = new NaturalRanking();
+    void testRanking1() {
+        final NaturalRanking ranking = new NaturalRanking();
         Assertions.assertArrayEquals(new double[] {1, 2, 3, 4}, ranking.apply(new double[] {5, 6, 7, 8}));
         Assertions.assertArrayEquals(new double[] {4, 1, 3, 2}, ranking.apply(new double[] {8, 5, 7, 6}));
-        Assertions.assertArrayEquals(new double[] {2.5, 1, 2.5, 4}, ranking.apply(new double[] {6, 5, 6, 7}));
+    }
+
+    @Test
+    void testRanking2() {
+        final double[] data = {8, 5, Double.NaN, 6};
+        final NaturalRanking ranking1 = new NaturalRanking();
+        Assertions.assertThrows(IllegalArgumentException.class, () -> ranking1.apply(data));
+        Assertions.assertArrayEquals(new double[] {4, 2, 1, 3}, new NaturalRanking(NaNStrategy.MINIMAL).apply(data));
+        Assertions.assertArrayEquals(new double[] {3, 1, 4, 2}, new NaturalRanking(NaNStrategy.MAXIMAL).apply(data));
+        Assertions.assertArrayEquals(new double[] {3, 1, 2}, new NaturalRanking(NaNStrategy.REMOVED).apply(data));
+        Assertions.assertArrayEquals(new double[] {3, 1, Double.NaN, 2}, new NaturalRanking(NaNStrategy.FIXED).apply(data));
+        final NaturalRanking ranking2 = new NaturalRanking(NaNStrategy.FAILED);
+        Assertions.assertThrows(IllegalArgumentException.class, () -> ranking2.apply(data));
+    }
+
+    @Test
+    void testRanking3() {
+        final double[] data = {7, 5, 7, 6};
+        Assertions.assertArrayEquals(new double[] {3.5, 1, 3.5, 2}, new NaturalRanking().apply(data));
+        Assertions.assertArrayEquals(new double[] {3, 1, 4, 2}, new NaturalRanking(TiesStrategy.SEQUENTIAL).apply(data));
+        Assertions.assertArrayEquals(new double[] {3, 1, 3, 2}, new NaturalRanking(TiesStrategy.MINIMUM).apply(data));
+        Assertions.assertArrayEquals(new double[] {4, 1, 4, 2}, new NaturalRanking(TiesStrategy.MAXIMUM).apply(data));
+        Assertions.assertArrayEquals(new double[] {3.5, 1, 3.5, 2}, new NaturalRanking(TiesStrategy.AVERAGE).apply(data));
+        final double[] r = new NaturalRanking(TiesStrategy.RANDOM).apply(data);
+        Assertions.assertTrue(Arrays.equals(new double[] {3, 1, 4, 2}, r) || Arrays.equals(new double[] {4, 1, 3, 2}, r));
+    }
+
+    @Test
+    void testRanking4() {
+        final double[] data = {7, 5, 7, 6};
+        final double[] r1 = new NaturalRanking(TiesStrategy.RANDOM).apply(data);
+        final double[] r2 = new NaturalRanking(new SplittableRandom()::nextLong).apply(data);
+        final UniformRandomProvider rng = RandomSource.KISS.create();
+        final double[] r3 = new NaturalRanking(rng::nextLong).apply(data);
+        final double[] expected1 = {3, 1, 4, 2};
+        final double[] expected2 = {4, 1, 3, 2};
+        Assertions.assertTrue(Arrays.equals(expected1, r1) || Arrays.equals(expected2, r1));
+        Assertions.assertTrue(Arrays.equals(expected1, r2) || Arrays.equals(expected2, r2));
+        Assertions.assertTrue(Arrays.equals(expected1, r3) || Arrays.equals(expected2, r3));
     }
 }
