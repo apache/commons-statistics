0f60efe0ecfba266ada8885e1c3efc5109abb7d9
AJoshi <janirudhg@gmail.com>
2023-07-14 21:50:54 +0530

    Changes - Fix formatting - Consolidate and remove redundant tests - Refactor TestHelper - Add `@since 1.1` to javadocs

--- commons-statistics-descriptive/src/main/java/org/apache/commons/statistics/descriptive/DoubleStatistic.java
+++ commons-statistics-descriptive/src/main/java/org/apache/commons/statistics/descriptive/DoubleStatistic.java
@@ -19,11 +19,13 @@ package org.apache.commons.statistics.descriptive;
 import java.util.function.DoubleConsumer;
 import java.util.function.DoubleSupplier;
 
-
 /**
  * Represents a state object for computing a single {@code Statistic} over {@code double} valued input(s).
  *
  * <p>Base interface implemented by all statistics.
+ *
+ * @since 1.1
  */
 public interface DoubleStatistic extends DoubleConsumer, DoubleSupplier {
+    // Composite interface
 }
